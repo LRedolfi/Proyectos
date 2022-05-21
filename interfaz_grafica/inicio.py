9f09b0b145a408417066de4e33c1c2debe6bf05d
Luciano <luchoredolfi@gmail.com>
2022-05-21 16:53:08 -0300

    Programa comentado

--- interfaz_grafica/inicio.py
+++ interfaz_grafica/inicio.py
@@ -1,21 +1,23 @@
-import pygame,sys
+import pygame,sys #Importo librerías
 
-pygame.init()
-ancho_ventana=500
-alto_ventana=400
-alto_rectángulo=20
+pygame.init() # Inicio pygame
+
+ancho_ventana=500 #Dimensiones de ventana   
+alto_ventana=400 
+alto_rectángulo=20 #Dimensiones de rectángulo
 ancho_rectángulo=130
-ventana=pygame.display.set_mode((ancho_ventana,alto_ventana))
-pygame.display.set_caption("Ventana")
-rectángulo = pygame.Rect(80, 100, ancho_rectángulo, alto_rectángulo)
 
-while True:
-    ventana.fill((51,123,255))
-    pygame.draw.rect(ventana,(141,51,255),rectángulo)
-    pygame.display.update()
-    tecla=pygame.key.get_pressed()
+ventana=pygame.display.set_mode((ancho_ventana,alto_ventana)) #Creo la ventana
+pygame.display.set_caption("Ventana") #Titulo de la ventana
+rectángulo = pygame.Rect(80, 100, ancho_rectángulo, alto_rectángulo) #Creo el rectángulo
 
-    if rectángulo.x<0:
+while True: #Ciclo infinito para que no se cierre la ventana
+    ventana.fill((51,123,255)) #Coloreo la ventana
+    pygame.draw.rect(ventana,(141,51,255),rectángulo) #Dibujo el rectángulo
+    pygame.display.update() #Actualizo la pantalla
+    tecla=pygame.key.get_pressed() #Capturo el evento tecla presionada
+#Chequeo que el rectángulo no salga de los limites de la ventana
+    if rectángulo.x<0: 
         rectángulo.x=0
     if rectángulo.x>ancho_ventana-ancho_rectángulo:
         rectángulo.x=ancho_ventana-ancho_rectángulo
@@ -23,16 +25,16 @@ while True:
         rectángulo.y=0
     if rectángulo.y>alto_ventana-alto_rectángulo:
         rectángulo.y=alto_ventana-alto_rectángulo
-
-    if tecla[pygame.K_a]:
+#Según la tecla presionada muevo el rectángulo
+    if tecla[pygame.K_a] or tecla[pygame.K_LEFT]:
         rectángulo.x=rectángulo.x-1
-    if tecla[pygame.K_d]:
+    if tecla[pygame.K_d] or tecla[pygame.K_RIGHT]:
         rectángulo.x=rectángulo.x+1
-    if tecla[pygame.K_s]:
+    if tecla[pygame.K_s] or tecla[pygame.K_DOWN]:
         rectángulo.y=rectángulo.y+1
-    if tecla[pygame.K_w]:
+    if tecla[pygame.K_w] or tecla[pygame.K_UP]:
         rectángulo.y=rectángulo.y-1
-
+#Si se presiona el botón cerrar de la ventana, salgo de la misma
     for evento in pygame.event.get():
         if evento.type==pygame.QUIT:
             pygame.quit()
